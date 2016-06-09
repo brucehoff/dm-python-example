d2a612d28f4a8e1342c670941a6364a87dca3896
bhoff <bruce.hoff@sagebase.org>
2016-06-09 15:42:43 -0700

    updated

--- train.py
+++ train.py
@@ -14,20 +14,19 @@ import sys
 if __name__ == '__main__':
     #PathDicom = "/trainingData"
     PathDicom = sys.argv[1]
-    for dirName, subdirList, fileList in os.walk(PathDicom):
-        for filename in fileList:
-            if ".dcm" in filename.lower():
-                sys.stdout.write("{}: ".format(filename))
-                RefDs = dicom.read_file(os.path.join(dirName,filename))
-                # RefDs.dir has lots of info
-                #print(RefDs.dir)
-                firsttime=True
-                sys.stdout.write('file size (MB): {}, '.format(os.path.getsize(dirName+"/"+filename)/1024000))
-                for label in ['PatientID', 'StudyDate', 'PatientAge', 'SeriesDescription', 'Rows', 'Columns']:
-                    if (firsttime):
-                        firsttime=False
-                    else:
-                        sys.stdout.write(', ')
-                    sys.stdout.write("{}: {}".format(label, RefDs.data_element(label).value))
-                print("")
-                
+    for filename in os.listdir(PathDicom):
+        if ".dcm" in filename.lower():
+            sys.stdout.write("{}: ".format(filename))
+            RefDs = dicom.read_file(os.path.join(PathDicom,filename))
+            # RefDs.dir has lots of info
+            #print(RefDs.dir)
+            firsttime=True
+            sys.stdout.write('file size (MB): {}, '.format(os.path.getsize(PathDicom+"/"+filename)/1024000))
+            for label in ['PatientID', 'StudyDate', 'PatientAge', 'SeriesDescription', 'Rows', 'Columns']:
+                if (firsttime):
+                    firsttime=False
+                else:
+                    sys.stdout.write(', ')
+                sys.stdout.write("{}: {}".format(label, RefDs.data_element(label).value))
+            print("")
+            
