b8df643a17a03206b0dd602a805b1e6a5c30d6ec
bhoff <bruce.hoff@sagebase.org>
2016-09-21 06:31:23 -0700

    updated scoring script

---
+++ score_sc1.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# a not-very-smart algorithm
+echo "subjectId	laterality	prediction" > /output/out.txt
+echo "abcd	L	0.001" >> /output/out.txt
+echo "abcd	R	0.010" >> /output/out.txt
--- test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# a not-very-smart algorithm
-echo "0 0" > /output/out.txt
